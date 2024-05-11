ef63d24d20e89d7503652ff08302f8a17f1d0ce7
HiHyerin <songhr95@gmail.com>
2024-05-12 00:10:16 +0900

    cyclList Date format 수정

--- src/main/java/io/swyp/luckybackend/luckyDays/dto/GetCyclListDto.java
+++ src/main/java/io/swyp/luckybackend/luckyDays/dto/GetCyclListDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Data
@@ -13,4 +15,24 @@ public class GetCyclListDto {
     private Long cyclNo;
     private Date startDt;
     private Date endDt;
+
+    public String getStartDt() {
+        return formatIsoDate(startDt.toString());
+    }
+
+    public String getEndDt() {
+        return formatIsoDate(endDt.toString());
+    }
+
+    private String formatIsoDate(String isoDate) {
+        SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
+        try {
+            Date date = inputFormat.parse(isoDate);
+            return outputFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
