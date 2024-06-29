02e5a4ba1b0606820237b7dcec44f27743ea4c18
JEONGMYEONGHONG <hkdhjm@gmail.com>
2024-06-29 23:10:57 +0900

    modified userNo -> oauthId

--- src/main/java/io/swyp/luckybackend/users/repository/UserRepository.java
+++ src/main/java/io/swyp/luckybackend/users/repository/UserRepository.java
@@ -2,6 +2,8 @@ package io.swyp.luckybackend.users.repository;
 
 import io.swyp.luckybackend.users.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,5 +14,6 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     UserEntity findByOauthId(String oauthId);
 
-    String findOauthIdByUserNo(Long userNo);
+    @Query("SELECT u.oauthId FROM UserEntity u WHERE u.userNo = :userNo")
+    String findOauthIdByUserNo(@Param("userNo") Long userNo);
 }
